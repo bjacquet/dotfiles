1a8913db7dc6e37ae29bc880349cb7c880cfcc0d
Bruno Jacquet <brunjact@protonmail.com>
2021-07-19 22:34:45 +0100

    Trying out Zim, replaces Prezto

---
+++ shell/zshenv.zsh
@@ -0,0 +1,12 @@
+# -*- mode: shell-script -*-
+#
+
+# Start configuration added by Zim install {{{
+#
+# User configuration sourced by all invocations of the shell
+#
+
+# Define Zim location
+: ${ZIM_HOME=${ZDOTDIR:-${HOME}}/Applications/zim}
+# }}} End configuration added by Zim install
+
