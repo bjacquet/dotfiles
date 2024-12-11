f5ab1cdcfbed7523531972ecd5217054f547e92f
Bruno Jacquet <bjacquet@komodo.local>
2024-12-11 22:56:32 +0100

    Add Bash completion on macOS

--- shell/functions/bash.sh
+++ shell/functions/bash.sh
@@ -2,3 +2,5 @@
 # -*- mode: shell-script -*-
 
 source $HOME/.config/dotfiles/shell/functions/highlight.sh
+source $HOME/.config/dotfiles/shell/functions/git-delete-done.sh
+source $HOME/.config/dotfiles/shell/functions/highlight.sh
