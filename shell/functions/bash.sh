b30b55895504ee0690272e8ac1cea5cd8afb0282
Bruno Jacquet <bjacquet@Mac.Home>
2025-01-07 09:18:24 +0100

    Reference function files correctly

--- shell/functions/bash.sh
+++ shell/functions/bash.sh
@@ -2,5 +2,5 @@
 # -*- mode: shell-script -*-
 
 source $HOME/.config/dotfiles/shell/functions/highlight.sh
-source $HOME/.config/dotfiles/shell/functions/git-delete-done.sh
-source $HOME/.config/dotfiles/shell/functions/highlight.sh
+source $HOME/.config/dotfiles/shell/functions/git-delete-done
+source $HOME/.config/dotfiles/shell/functions/git-rebase-master
