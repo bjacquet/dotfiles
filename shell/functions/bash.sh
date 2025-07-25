7401ebcdfa0d2fc8a280c7e6678ae7d30673fc4b
Bruno Jacquet <you@example.com>
2025-07-25 11:53:24 +0200

    Turning alias into functions

--- shell/functions/bash.sh
+++ shell/functions/bash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # -*- mode: shell-script -*-
 
-source $HOME/.config/dotfiles/shell/functions/highlight.sh
+source $HOME/.config/dotfiles/shell/functions/git-checkout-master-pull
 source $HOME/.config/dotfiles/shell/functions/git-delete-done
+source $HOME/.config/dotfiles/shell/functions/git-done
+source $HOME/.config/dotfiles/shell/functions/git-new-branch
 source $HOME/.config/dotfiles/shell/functions/git-rebase-master
+source $HOME/.config/dotfiles/shell/functions/highlight.sh
